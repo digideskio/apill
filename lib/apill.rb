a6027b205aea822696f2bdac8c11225d94a4b758
Jeff Felchner <accounts+git@thekompanee.com>
2014-04-15 23:31:57 -0500

    Add require paths to apill so that we can require apill and autoload the rest of the dependencies

--- lib/apill.rb
+++ lib/apill.rb
@@ -1,4 +1,7 @@
 require 'apill/version'
 
-module Apill
-end
+require 'apill/matchers/accept_header_matcher'
+require 'apill/matchers/invalid_api_request_matcher'
+require 'apill/matchers/version_matcher'
+
+require 'apill/responses/invalid_api_request_response'
