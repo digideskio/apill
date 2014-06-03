b82f2d0fd5290e91ab05ea05dfc6ebcee738c4e6
Jeff Felchner and Drew Montgomery <pair+hhd418-jfelchner@example.com>
2014-06-03 16:22:15 -0500

    Feature: Add RescuableResource module
    We started noticing similar patterns throughout the controllers, where, for
    example, each `update` action would have to throw an error if the model to
    update did not exist.
    
    In order to refactor that, we extracted the logic out so that the controller
    would rescue a specific error (in this case `ResourceNotFound`) from any action.
    
    In the end we had three `rescue_from` blocks, and again, after seeing them we
    noticed that the blocks could be generalized enough that we didn't have to copy
    and paste them into each controller.
    
    Our solution was to create this `RescuableResource` module, which can be enabled
    like so:
    
    ```ruby
    class MyController < ApplicationController
      include Apill::RescuableResource
    
      rescue_resource 'my_resource_name', from: %w{errors you want rescued},
                                          via:  HumanError::ErrorLookupClass
    end
    ```
    
    The reason we need the `via` option is because each individual engine has its
    own configuration for `HumanError` errors and so we can pass in that 'lookup'
    class into the `rescue_from` blocks so that each error that ends up being
    raised/generated is wrapped with that configuration.

--- lib/apill/rescuable_resource.rb
+++ lib/apill/rescuable_resource.rb
@@ -1,4 +1,56 @@
+require 'human_error'
+
 module  Apill
 module  RescuableResource
+  module ClassMethods
+    def rescue_resource(resource_name, from:, via:)
+      lookup_library = via
+
+      if from.include? 'persistence'
+        rescue_from HumanError::Errors::ResourcePersistenceError do |e|
+          error = lookup_library.fetch(
+                  'ResourcePersistenceError',
+                  resource_name: e.resource_name,
+                  attributes:    e.attributes,
+                  errors:        e.errors,
+                  action:        action_name)
+
+          render json:   error,
+                 status: error.http_status
+        end
+      end
+
+      if from.include? 'not_found'
+        rescue_from HumanError::Errors::ResourceNotFoundError do |e|
+          error = lookup_library.fetch(
+                  'ResourceNotFoundError',
+                  resource_name: e.resource_name,
+                  resource_id:   e.resource_id,
+                  action:        action_name)
+
+          render json:   error,
+                 status: error.http_status
+        end
+      end
+
+      if from.include? 'association'
+        rescue_from HumanError::Errors::AssociationError do |e|
+          error = lookup_library.fetch(
+                  'AssociationError',
+                  resource_name:    e.resource_name,
+                  association_name: e.association_name,
+                  association_id:   e.association_id,
+                  attributes:       e.attributes)
+
+          render json:   error,
+                 status: error.http_status
+        end
+      end
+    end
+  end
+
+  def self.included(base)
+    base.extend ClassMethods
+  end
 end
 end
