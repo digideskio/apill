a6e271077af40d50066f348881e7e7e5f6b30f2c
Jeff Felchner <accounts+git@thekompanee.com>
2014-11-05 22:54:33 -0600

    Feature: Add ApiRequest middleware

---
+++ lib/apill/middleware/api_request.rb
@@ -0,0 +1,32 @@
+require 'json'
+require 'apill/configuration'
+require 'apill/matchers/subdomain_matcher'
+require 'apill/matchers/invalid_api_request_matcher'
+require 'apill/responses/invalid_api_request_response'
+require 'apill/responses/invalid_subdomain_response'
+
+module  Apill
+module  Middleware
+class   ApiRequest
+  def initialize(app)
+    @app = app
+  end
+
+  def call(env)
+    env['API_APPLICATION_NAME'] = Apill.configuration.application_name
+
+    if Matchers::SubdomainMatcher.new(allowed_subdomains: Apill.configuration.allowed_subdomains).
+                                  matches?(env)
+
+      if Matchers::AcceptHeaderMatcher.new.matches?(env)
+        @app.call(env)
+      else
+        Responses::InvalidApiRequestResponse.call(env)
+      end
+    else
+      Responses::InvalidSubdomainResponse.call(env)
+    end
+  end
+end
+end
+end
---
+++ spec/apill/middleware/api_request_spec.rb
@@ -0,0 +1,114 @@
+require 'rspectacular'
+require 'apill/middleware/api_request'
+
+HumanError.configure do |config|
+  config.api_error_documentation_url = 'http://error.com'
+  config.knowledgebase_url           = 'http://knowledge.com'
+  config.api_version                 = '1'
+end
+
+Apill.configure do |config|
+  config.allowed_subdomains = %w{api}
+  config.application_name   = 'matrix'
+end
+
+module    Apill
+module    Middleware
+describe  ApiRequest do
+  let(:app) { lambda { |env| [200, {}, 'response'] } }
+
+  it 'does not allow requests if they are not for an allowed subdomain' do
+    api_request_middleware = ApiRequest.new(app)
+
+    request = {
+      'HTTP_HOST'            => 'notvalid.example.com',
+      'HTTP_ACCEPT'          => '',
+      'QUERY_STRING'         => 'first=my_param&accept=application/vnd.silent+zion;version=1.0.0',
+      'API_APPLICATION_NAME' => 'matrix',
+    }
+
+    status, headers, response = api_request_middleware.call(request)
+
+    expect(status).to   eql 404
+    expect(headers).to  eql Hash.new
+    expect(response).to eql(
+      [
+        '{' \
+          '"error":' \
+          '{' \
+            '"status":404,' \
+            '"code":1010,' \
+            '"developer_documentation_uri":"http://error.com/1010?version=1",' \
+            '"customer_support_uri":"http://knowledge.com/1234567890",' \
+            '"developer_message_key":"errors.invalid.subdomain.error.developer",' \
+            '"developer_message":"The resource you attempted to access is either not authorized for the authenticated user or does not exist.",' \
+            '"developer_details":' \
+              '{' \
+                '"http_host":"notvalid.example.com"' \
+              '},' \
+            '"friendly_message_key":"errors.invalid.subdomain.error.friendly",' \
+            '"friendly_message":"Sorry! The resource you tried to access does not exist."' \
+          '}' \
+        '}'
+      ]
+    )
+  end
+
+  it 'does not allow requests if they are for an allowed subdomain but does ' \
+     'not have a valid accept header' do
+
+    api_request_middleware = ApiRequest.new(app)
+
+    request = {
+      'HTTP_HOST'            => 'api.example.com',
+      'HTTP_ACCEPT'          => '',
+      'QUERY_STRING'         => 'first=my_param&accept=application/vnd.silent+zion;version=1.0.0',
+      'API_APPLICATION_NAME' => 'matrix',
+    }
+
+    status, headers, response = api_request_middleware.call(request)
+
+    expect(status).to   eql 400
+    expect(headers).to  eql Hash.new
+    expect(response).to eql(
+      [
+        '{' \
+          '"error":' \
+          '{' \
+            '"status":400,' \
+            '"code":1007,' \
+            '"developer_documentation_uri":"http://error.com/1007?version=1",' \
+            '"customer_support_uri":"http://knowledge.com/1234567890",' \
+            '"developer_message_key":"errors.invalid.api.request.error.developer",' \
+            '"developer_message":"The accept header that you passed in the request cannot be parsed, please refer to the documentation to verify.",' \
+            '"developer_details":' \
+              '{' \
+                '"accept_header":""' \
+              '},' \
+            '"friendly_message_key":"errors.invalid.api.request.error.friendly",' \
+            '"friendly_message":"Sorry! We couldn\'t understand what you were trying to ask us to do."' \
+          '}' \
+        '}'
+      ]
+    )
+  end
+
+  it 'does allow requests if both the subdomain and the accept header are valid' do
+    api_request_middleware = ApiRequest.new(app)
+
+    request = {
+      'HTTP_HOST'            => 'api.example.com',
+      'HTTP_ACCEPT'          => 'application/vnd.matrix+zion;version=1.0.0',
+      'QUERY_STRING'         => 'first=my_param&accept=application/vnd.matrix+zion;version=1.0.0',
+      'API_APPLICATION_NAME' => 'matrix',
+    }
+
+    status, headers, response = api_request_middleware.call(request)
+
+    expect(status).to   eql 200
+    expect(headers).to  eql Hash.new
+    expect(response).to eql 'response'
+  end
+end
+end
+end
