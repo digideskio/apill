98d12a4a28770629508f49087d13629465eec929
Jeff Felchner <accounts+git@thekompanee.com>
2014-11-05 23:50:07 -0600

    Bugfix: Add missing require statements
    I forgot to add the new require statements for the new classes

--- lib/apill.rb
+++ lib/apill.rb
@@ -1,8 +1,12 @@
 require 'apill/version'
 
+require 'apill/configuration'
 require 'apill/matchers/accept_header_matcher'
 require 'apill/matchers/invalid_api_request_matcher'
-require 'apill/matchers/version_matcher'
 require 'apill/matchers/subdomain_matcher'
+require 'apill/matchers/version_matcher'
+
+require 'apill/middleware/api_request'
 
 require 'apill/responses/invalid_api_request_response'
+require 'apill/responses/invalid_subdomain_response'
