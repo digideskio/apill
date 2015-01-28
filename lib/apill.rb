1cacf87af99c2b1038db7805335b8e521c36d425
Jeff Felchner <accounts+git@thekompanee.com>
2015-01-28 15:09:51 -0600

    Removed: require line for a file which no longer exists
    This was accidentally left in.

--- lib/apill.rb
+++ lib/apill.rb
@@ -4,7 +4,6 @@ require 'apill/version'
 
 require 'apill/configuration'
 require 'apill/matchers/accept_header_matcher'
-require 'apill/matchers/invalid_api_request_matcher'
 require 'apill/matchers/subdomain_matcher'
 require 'apill/matchers/version_matcher'
 require 'apill/mixins/sortable'
