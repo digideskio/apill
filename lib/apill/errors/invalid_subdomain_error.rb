9cfdda55fac337dae924b3107b75c73cf5666466
Jeff Felchner <accounts+git@thekompanee.com>
2015-08-11 03:50:18 -0500

    Changed: Error classes for the new version of human_error

--- lib/apill/errors/invalid_subdomain_error.rb
+++ lib/apill/errors/invalid_subdomain_error.rb
@@ -2,24 +2,26 @@ require 'human_error'
 
 module  Apill
 module  Errors
-class   InvalidSubdomainError < HumanError::Errors::RequestError
+class   InvalidSubdomainError < RuntimeError
+  include HumanError::Error
+
   attr_accessor :http_host
 
   def http_status
     404
   end
 
-  def developer_message
+  def title
+    'Invalid Subdomain'
+  end
+
+  def detail
     'The resource you attempted to access is either not authorized for the ' \
     'authenticated user or does not exist.'
   end
 
-  def developer_details
+  def source
     { http_host: http_host }
-  end
-
-  def friendly_message
-    'Sorry! The resource you tried to access does not exist.'
   end
 end
 end
--- spec/apill/errors/invalid_api_request_error_spec.rb
+++ spec/apill/errors/invalid_api_request_error_spec.rb
@@ -10,29 +10,20 @@ describe  InvalidApiRequestError do
     expect(error.http_status).to eql 400
   end
 
-  it 'has a code of 1007' do
-    expect(error.code).to eql 1007
+  it 'has a code' do
+    expect(error.code).to eql 'errors.invalid_api_request_error'
   end
 
-  it 'has a knowledgebase article ID of 1234567890' do
-    expect(error.knowledgebase_article_id).to eql '1234567890'
+  it 'can output the detail' do
+    expect(error.detail).to eql 'The accept header that you passed in the ' \
+                                'request cannot be parsed, please refer to ' \
+                                'the documentation to verify.'
   end
 
-  it 'can output the developer message' do
-    expect(error.developer_message).to eql 'The accept header that you passed in the ' \
-                                           'request cannot be parsed, please refer to ' \
-                                           'the documentation to verify.'
-  end
-
-  it 'can output the developer details' do
+  it 'can output the source' do
     error = InvalidApiRequestError.new accept_header: 'foo'
 
-    expect(error.developer_details).to eql(accept_header: 'foo')
-  end
-
-  it 'can output the friendly message' do
-    expect(error.friendly_message).to eql "Sorry! We couldn't understand what you were " \
-                                          'trying to ask us to do.'
+    expect(error.source).to eql(accept_header: 'foo')
   end
 end
 end
