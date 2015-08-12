724fe04ee479a295fbd8d6436ce11f5f3fd2de8f
Jeff Felchner <accounts+git@thekompanee.com>
2015-08-11 22:49:49 -0500

    Changed: Resource to be a meta module
    Change-Id: Ie8141bc01ec05617260a1429ecaa789049aa5dff

--- lib/apill/resource.rb
+++ lib/apill/resource.rb
@@ -1,68 +1,13 @@
-require 'apill/resource/model'
+require 'apill/processable_resource'
 require 'human_error/rescuable_resource'
+require 'human_error/verifiable_resource'
 
 module  Apill
 module  Resource
-  module ClassMethods
-    def plural_resource_name
-      name[/(\w+)Controller\z/, 1].
-      underscore.
-      pluralize.
-      downcase
-    end
-
-    def singular_resource_name
-      name[/(\w+)Controller\z/, 1].
-      underscore.
-      singularize.
-      downcase
-    end
-  end
-
   def self.included(base)
-    base.extend  ClassMethods
+    base.include Apill::ProcessableResource
     base.include HumanError::RescuableResource
     base.include HumanError::VerifiableResource
-  end
-
-  def api_resource
-    @resource ||= Resource::Model.new(
-                    resource:   public_send(self.class.plural_resource_name),
-                    parameters: api_resource_params)
-  end
-
-  def api_resource_params
-    params.permit(:sort,
-                  page:   %i{
-                    number
-                    size
-                    offset
-                    limit
-                    cursor
-                  },
-                  filter: api_filterable_parameters)
-  end
-
-  def api_filterable_parameters
-    @api_filterable_parameters ||= begin
-      filter_params     = params.fetch(:filter, {})
-      scalar_params     = [:query]
-      array_params      = {}
-
-      api_filterable_attributes.each do |api_filterable_attribute|
-        if filter_params[api_filterable_attribute].class == Array
-          array_params[api_filterable_attribute] = []
-        else
-          scalar_params << api_filterable_attribute
-        end
-      end
-
-      scalar_params << array_params
-    end
-  end
-
-  def api_filterable_attributes
-    []
   end
 end
 end
