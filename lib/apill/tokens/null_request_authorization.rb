62eb8efb280da37ff4af24814410232599a9cdd8
Jeff Felchner <accounts+git@thekompanee.com>
2015-10-07 15:38:32 -0500

    Added: Tokens::NullAuthorization
    This token class encapsulates what it means for the token to not have been
    passed into the request.
    
    --------------------------------------------------------------------------------
    Signed-off-by: Jeff Felchner <accounts+git@thekompanee.com>
    
    Change-Id: I3590bb802375f49969812bdbe7036c17b5d05503

---
+++ lib/apill/tokens/null_request_authorization.rb
@@ -0,0 +1,21 @@
+require 'singleton'
+
+module  Apill
+module  Tokens
+class   NullRequestAuthorization
+  include Singleton
+
+  def valid?
+    true
+  end
+
+  def blank?
+    true
+  end
+
+  def to_h
+    {}
+  end
+end
+end
+end
