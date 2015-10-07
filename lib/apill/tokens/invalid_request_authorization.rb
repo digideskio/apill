86eb441be1b94641ec71d29d5654361934924b36
Jeff Felchner <accounts+git@thekompanee.com>
2015-10-07 15:37:18 -0500

    Added: Tokens::InvalidRequestAuthorization
    This will capture what it means for a user to pass a token in which is invalid
    for any reason.
    
    --------------------------------------------------------------------------------
    Signed-off-by: Jeff Felchner <accounts+git@thekompanee.com>
    
    Change-Id: Ie65597ea2b4cf86df4e96c864343af5a48cc4633

---
+++ lib/apill/tokens/invalid_request_authorization.rb
@@ -0,0 +1,21 @@
+require 'singleton'
+
+module  Apill
+module  Tokens
+class   InvalidRequestAuthorization
+  include Singleton
+
+  def valid?
+    false
+  end
+
+  def blank?
+    false
+  end
+
+  def to_h
+    {}
+  end
+end
+end
+end
