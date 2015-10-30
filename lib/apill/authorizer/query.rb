fa6985d630a33c35862d747744dc44722ba596c2
Jeff Felchner <accounts+git@thekompanee.com>
2015-10-30 00:48:50 -0700

    Added: token, user and resource to Authorizer::Query
    These are the main things a general authorizer is going to need to determine
    whether a user/token can access a specific resource.
    
    * `token` is the JSON Web Token data that was used for the request. This will
      typically contain the roles that the user has (eg normal, admin, etc).
    * `user` is the actual user object which was looked up for the request
    * `resource` is the object which the authorization is requesting access to
    
    The `Query` is also set up so that subclasses may add additional parameters to
    the initializer simply by passing them in and adding an `attr_accessor` on the
    subclass.
    
    --------------------------------------------------------------------------------
    Branch:        feature/add-authorization-logic
    Signed-off-by: Jeff Felchner <accounts+git@thekompanee.com>
    
    Change-Id: I05e4c32d31c2a156acc38517c94fce5ae6c404a3

--- lib/apill/authorizer/query.rb
+++ lib/apill/authorizer/query.rb
@@ -1,6 +1,19 @@
 module  Apill
 module  Authorizer
 class   Query
+  attr_accessor :token,
+                :user,
+                :resource
+
+  def initialize(token:, user:, resource:, **other)
+    self.token    = token
+    self.user     = user
+    self.resource = resource
+
+    other.each do |name, value|
+      self.public_send("#{name}=", value)
+    end
+  end
 end
 end
 end
