25cd678a3d96a522bd390cab25e00319771504ac
Jeff Felchner <accounts+git@thekompanee.com>
2015-10-30 00:52:34 -0700

    Added: Default queries to Authorizer::Query
    The typical CRUD action queries have been added.  By default none of them are
    authorized with the exception of `index`.  `index` is authorized because very
    rarely will a user _not_ be allowed to access and `index`. The authorization of
    `index` usually comes in the form of _what_ is listed (the scope) rather than
    _can_ I list something.  See a later commit regarding the scope.
    
    --------------------------------------------------------------------------------
    Branch:        feature/add-authorization-logic
    Signed-off-by: Jeff Felchner <accounts+git@thekompanee.com>
    
    Change-Id: I68bfaa1ef72a21222f15569a2feee0925c5518d7

--- lib/apill/authorizer/query.rb
+++ lib/apill/authorizer/query.rb
@@ -14,6 +14,26 @@ class   Query
       self.public_send("#{name}=", value)
     end
   end
+
+  def able_to_index?
+    true
+  end
+
+  def able_to_show?
+    false
+  end
+
+  def able_to_create?
+    false
+  end
+
+  def able_to_update?
+    false
+  end
+
+  def able_to_destroy?
+    false
+  end
 end
 end
 end
--- spec/apill/authorizer/query_spec.rb
+++ spec/apill/authorizer/query_spec.rb
@@ -4,6 +4,17 @@ require 'apill/authorizer/query'
 module    Apill
 module    Authorizer
 describe  Query do
+  it 'does not authorize the resource by default' do
+    authorizer = Query.new(token:    '123',
+                           user:     'my_user',
+                           resource: 'my_resource')
+
+    expect(authorizer).to     be_able_to_index
+    expect(authorizer).not_to be_able_to_show
+    expect(authorizer).not_to be_able_to_create
+    expect(authorizer).not_to be_able_to_update
+    expect(authorizer).not_to be_able_to_destroy
+  end
 end
 end
 end
