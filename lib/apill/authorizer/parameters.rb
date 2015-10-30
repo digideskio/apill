855a6fede082a4370963bf302426b34d2eb103a3
Jeff Felchner <accounts+git@thekompanee.com>
2015-10-30 01:00:29 -0700

    Added: token, user and params to Authorizer::Parameter
    These are the main things a parameter authorizer is going to need to determine
    which parameters a user/token have access to.
    
    * `token` is the JSON Web Token data that was used for the request. This will
      typically contain the roles that the user has (eg normal, admin, etc).
    * `user` is the actual user object which was looked up for the request
    * `params` are typically the params passed into the controller from the request
    
    The `Parameter` is also set up so that subclasses may add additional parameters
    to the initializer simply by passing them in and adding an `attr_accessor` on
    the subclass.
    
    --------------------------------------------------------------------------------
    Branch:        feature/add-authorization-logic
    Signed-off-by: Jeff Felchner <accounts+git@thekompanee.com>
    
    Change-Id: Id2262900be59becbfc404bd6af6e256abf3395b9

--- lib/apill/authorizer/parameters.rb
+++ lib/apill/authorizer/parameters.rb
@@ -1,6 +1,19 @@
 module  Apill
 module  Authorizer
 class   Parameters
+  attr_accessor :token,
+                :user,
+                :params
+
+  def initialize(token:, user:, params:, **other)
+    self.token  = token
+    self.user   = user
+    self.params = params
+
+    other.each do |name, value|
+      self.public_send("#{name}=", value)
+    end
+  end
 end
 end
 end
