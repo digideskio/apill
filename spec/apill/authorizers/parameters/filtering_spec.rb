e87364ff8f92272b42a04bdbd4220728f539f8f2
Jeff Felchner <accounts+git@thekompanee.com>
2015-11-01 14:47:30 -0800

    Added: parameter processing to Authorizers::Parameters::Filtering
    This allows the user to have a really easy way to add filtering parameters that
    are allowed to be passed in by the user via `add_filtering_parameters` or the
    singular version `add_filtering_parameter`.
    
    These methods know how to put a given allowed parameter onto the `filter` params
    hash.  It also knows to put an allowed value of an array if the parameter is
    being passed in as an array.  This allows for the users of the API to pass in
    a scalar value or as an `Array` which is necessary in a lot of cases.
    
    ```ruby
    params = { :foo => 'bar', :baz => [ :qux, :zaz ] }
    ```
    
    Then the value generated to be passed to `strong_parameters` will be:
    
    ```ruby
    [
      :foo,
      {
        baz: [],
      },
    ]
    ```
    
    --------------------------------------------------------------------------------
    Branch:        feature/add-authorization-logic
    Signed-off-by: Jeff Felchner <accounts+git@thekompanee.com>
    
    Change-Id: I72c1f4386ec56917dedcd83349682beddac20a52

--- spec/apill/authorizers/parameters/filtering_spec.rb
+++ spec/apill/authorizers/parameters/filtering_spec.rb
@@ -5,6 +5,65 @@ module    Apill
 module    Authorizers
 class     Parameters
 describe  Filtering do
+  let(:params) { { filter: { name: 'Bill', age: 26 } } }
+
+  it 'can authorize new filter parameters', verify: false do
+    filter_params = Filtering.new(token:  '1234',
+                                  user:   '1234',
+                                  params: params)
+
+    allow(params).to receive(:permit)
+
+    filter_params.send(:add_filterable_parameters, :name, :age)
+    filter_params.call
+
+    expect(params).to have_received(:permit).
+                      with(:sort, include(filter: include(:name, :age)))
+  end
+
+  it 'can authorize parameters if they come in as arrays', verify: false do
+    params        = {
+                      filter: {
+                        name: 'Bill',
+                        ary:  %w{hello},
+                      }
+                    }
+    filter_params = Filtering.new(token:  '1234',
+                                  user:   '1234',
+                                  params: params)
+
+    allow(params).to receive(:permit)
+
+    filter_params.send(:add_filterable_parameters, :name, :ary)
+    filter_params.call
+
+    expect(params).to have_received(:permit).
+                      with(:sort, include(filter: include(:name, ary: [])))
+  end
+
+  it 'has default authorized parameters', verify: false do
+    filter_params = Filtering.new(token:  '1234',
+                                  user:   '1234',
+                                  params: params)
+
+    allow(params).to receive(:permit)
+
+    filter_params.call
+
+    expect(params).to have_received(:permit).
+                      with(:sort,
+                           page:   %i{
+                             number
+                             size
+                             offset
+                             limit
+                             cursor
+                           },
+                           filter: [
+                             :query,
+                             {}
+                           ])
+  end
 end
 end
 end
